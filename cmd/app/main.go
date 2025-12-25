4087990da7ba930dfc3425eb0862e6ac17d6fb18
Justin-dev-git <maijustin95@gmail.com>
2025-12-25 17:52:54 +0100

    basic Go variables example

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Println("Hello, this is my first structured Go-Project")
+	a :=10
+	b := 3.14
+	var c bool = true
+	var d string ="Hello World"
+	
+	fmt.Println(a, b, c, d)
 }
