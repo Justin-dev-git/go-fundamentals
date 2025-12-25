f0628ec4fa5637b63ea568beb7dafb55fdbe3bfc
Justin-dev-git <maijustin95@gmail.com>
2025-12-25 17:59:12 +0100

    basic if, else, else if commands in GO

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,10 +3,12 @@ package main
 import "fmt"
 
 func main(){
-	a :=10
-	b := 3.14
-	var c bool = true
-	var d string ="Hello World"
 	
-	fmt.Println(a, b, c, d)
+	c := false
+	d := true
+	if c{
+	fmt.Println(c)
+	}else{
+	fmt.Println(!d)
+	}
 }
