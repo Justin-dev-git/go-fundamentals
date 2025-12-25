56775c6e57466bac34baeeaed5b674d89cf54da2
Justin-dev-git <maijustin95@gmail.com>
2025-12-25 19:52:59 +0100

    basic slices in GO

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,27 +3,12 @@ package main
 import "fmt"
 
 func main(){
-	/*
-	for i:=0; i<5;i++{
-		fmt.Println("Zähler: ", i)
+	numbers := []int{1,2,3,4}
+	numbers = append(numbers, 5)
+	sum :=0
+	for i, value := range numbers{
+		fmt.Println("Index:", i, "Value", value)
+		sum+=value	
 	}
-	i :=0
-	for i<3{
-		fmt.Println("Zähler: ",i+5)
-		i++
-	}
-	for i:=0;i<10;i++{
-		if i==4{
-			break
-		}
-		fmt.Println("Zähler: ", i+8)
-	}*/
-	var numbers [3]int
-	numbers[0] = 1
-	numbers[1] = 12
-	numbers[2] = 1
-	numbers2 :=[3]int{1,2,3}
-
-	fmt.Print(numbers)
-	fmt.Println(numbers2)
+	fmt.Println(sum)
 }
