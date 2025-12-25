9c1f6e70f6b99cb417b37f6a6e0638620784f8b4
Justin-dev-git <maijustin95@gmail.com>
2025-12-25 19:38:50 +0100

    basic for-loops in GO

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -7,4 +7,15 @@ func main(){
 	for i:=0; i<5;i++{
 		fmt.Println("Zähler: ", i)
 	}
+	i :=0
+	for i<3{
+		fmt.Println("Zähler: ",i+5)
+		i++
+	}
+	for i:=0;i<10;i++{
+		if i==4{
+			break
+		}
+		fmt.Println("Zähler: ", i+8)
+	}
 }
