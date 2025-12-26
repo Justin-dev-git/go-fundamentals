e49022492bf607933c76ca54d0bf8117a65eecb4
Justin-dev-git <maijustin95@gmail.com>
2025-12-26 10:18:49 +0100

    apply gofmt formatting

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -4,30 +4,32 @@ import (
 	"fmt"
 	"os"
 )
-type Server struct{
-	Name string
+
+type Server struct {
+	Name   string
 	Active bool
 }
-func (s *Server) Start(){
-	s.Active=true
+
+func (s *Server) Start() {
+	s.Active = true
 }
-func (s *Server) Stop(){
-	s.Active=false
+func (s *Server) Stop() {
+	s.Active = false
 }
-func main(){
+func main() {
 	server := &Server{Name: "Server 1"}
-	if len(os.Args)<2{
+	if len(os.Args) < 2 {
 		fmt.Println("Usage:app start|stop")
 		return
 	}
 	command := os.Args[1]
-	if command == "start"{
-	server.Start()
-	fmt.Println("Server wird gestartet")
-	} else if command == "stop"{
-	server.Stop()
-	fmt.Println("Server wird gestoppt")
-	} else{
-	fmt.Println("Unbekanntes Kommando", command)
+	if command == "start" {
+		server.Start()
+		fmt.Println("Server wird gestartet")
+	} else if command == "stop" {
+		server.Stop()
+		fmt.Println("Server wird gestoppt")
+	} else {
+		fmt.Println("Unbekanntes Kommando", command)
 	}
 }
