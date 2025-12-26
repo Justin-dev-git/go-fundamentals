f9cd259164582f6baeaa48a3404089995311428f
Justin-dev-git <maijustin95@gmail.com>
2025-12-26 17:05:42 +0100

    practice slice usage and iterative number conversion

--- main.go
+++ main.go
@@ -2,6 +2,22 @@ package main
 
 import "fmt"
 
-func main(){
-	fmt.Println("Hallo Welt")
+func main() {
+	var Zahl []int
+	i := 1101
+	for i > 0 {
+		Zahl = append(Zahl, i%2)
+		i /= 2
+	}
+	fmt.Println(Zahl)
+	s := 0.375
+	var Zahl2 []int
+	for s != 1 {
+		Zahl2 = append(Zahl2, int(s*2))
+		s *= 2
+		if s > 1 {
+			s -= 1
+		}
+	}
+	fmt.Println(Zahl2)
 }
